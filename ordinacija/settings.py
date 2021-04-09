4fd4f8f63c4056b3376f08932dd1e07e5d237008
Miloš <milos.vukosavljevicc@gmail.com>
2021-04-09 13:53:19 +0200

    treci

--- ordinacija/settings.py
+++ ordinacija/settings.py
@@ -136,7 +136,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.Compressed.ManifestStaticFilesStorage'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'majlo1000@gmail.com'
-EMAIL_HOST_PASSWORD = 'rojkovac'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = 'True'
 
 
