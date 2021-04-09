84a05839f5f03aa44074570f0174059fef62605e
Miloš <milos.vukosavljevicc@gmail.com>
2021-04-09 12:25:30 +0200

    heroku dodat2

--- ordinacija/settings.py
+++ ordinacija/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiffleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ordinacija.urls'
