62d2154125b49f5add38dc22ce207e29fb81881e
Miloš <milos.vukosavljevicc@gmail.com>
2021-04-09 11:52:37 +0200

    heroku dodat

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ordinacija.wsgi
--- ordinacija/settings.py
+++ ordinacija/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiffleware',
 ]
 
 ROOT_URLCONF = 'ordinacija.urls'
@@ -125,6 +129,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.Compressed.ManifestStaticFilesStorage'
+
 #email settings
 
 EMAIL_HOST = 'smtp.gmail.com'
@@ -132,3 +138,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'majlo1000@gmail.com'
 EMAIL_HOST_PASSWORD = 'rojkovac'
 EMAIL_USE_TLS = 'True'
+
+
+django_heroku.settings(locals())
