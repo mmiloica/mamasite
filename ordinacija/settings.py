2c4dce9197dd5f4ef57af4cc5be4a7035f96f85b
Miloš <milos.vukosavljevicc@gmail.com>
2021-04-21 18:02:25 +0200

    secretkey

--- ordinacija/settings.py
+++ ordinacija/settings.py
@@ -31,13 +31,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = env('SECRET_KEY')
+#SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY='!8^*3unp^nq=qq9+*1c@2*)=$+!%#a^^tb&p53-womrk^9wgbx'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['www.decijilekar.rs']
+ALLOWED_HOSTS = []
 
 
 # Application definition
